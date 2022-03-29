d9ae255118cbdacb53624467b33078f2113ca4fe
John Sangster <john.sangster445@gmail.com>
2022-03-29 17:14:40 -0400

    test

--- app.py
+++ app.py
@@ -18,5 +18,27 @@ def seeposts():
 if __name__ == "__main__":
     app.run()
 
+def writeToMarkers(message):
+    m = open("marker_count.txt", "r")
+    n = open("markers.txt", "r")
+
+    current_m = int(m.read())
+    current_n = n.read()
+
+    m.close()
+    n.close()
+
+    new_m = current_m + 1
+    new_n = current_n + "\n" + message
+
+    m = open("marker_count.txt", "w")
+    n = open("markers.txt", "w")
+
+    m.write(new_m)
+    n.write(new_n)
+
+    m.close()
+    n.close()
+
 # The goal of this app is to show messages sent across space, not across time
 # Messages are organized according to the lat/long of where they were sent from (rounded to int)

d79f898c6a683489c60a3fc4ee9e455895e157b9
John Sangster <john.sangster445@gmail.com>
2022-04-02 18:56:41 -0400

    change button type

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, url_for, request
 
+# The goal of this app is to show messages sent across space, not across time
+# Messages are organized according to the lat/long of where they were sent from (rounded to int)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -66,6 +69,3 @@ def display():
 
 if __name__ == "__main__":
     app.run()
-
-# The goal of this app is to show messages sent across space, not across time
-# Messages are organized according to the lat/long of where they were sent from (rounded to int)
